e14146bfd915953e7cb890a0eec0023160cf1001
Mark Dueri <mark.dueri@gmail.com>
2021-12-26 10:37:12 -0700

    add files

---
+++ Jack3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jack3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Jack j = new Jack();
+            j.height = 40;
+            j.weight = 80;
+
+
+            Jack j2 = new Jack();
+                j.eyecolor = "Brown";
+
+            Console.WriteLine(j.eyecolor);
+            Console.WriteLine(j2.eyecolor);
+
+            Console.ReadKey();
+        }
+    }
+
+
+    public class Jack
+    {
+        public int weight = 0;
+        public int height = 0;
+        public string eyecolor = "blue";
+
+        
+    }
+
+}
